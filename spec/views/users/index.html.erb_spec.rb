8504304aefb801ec30d243bbb4d45cd28eb08be9
@an280420 <angit280420@gmail.com>
2021-03-07 13:01:45 +0500

    Added test for views users/index

---
+++ spec/views/users/index.html.erb_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe 'users/index', type: :view do
+  before(:each) do
+  end
+
+  it 'renders player names' do
+  end
+
+  it 'renders player balances' do
+  end
+
+  it 'renders player names in right order' do
+  end
+end
